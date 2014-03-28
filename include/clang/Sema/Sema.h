553a94067a512fc9d400dc629831912d7df333a7
Vassil Vassilev <vvasilev@cern.ch>
2014-03-28 10:48:12 +0100

    Access sema internals and revert the list of unused decls.

--- include/clang/Sema/Sema.h
+++ include/clang/Sema/Sema.h
@@ -10389,6 +10389,7 @@ protected:
   friend class ASTReader;
   friend class ASTDeclReader;
   friend class ASTWriter;
+  friend class DeclUnloader;
 
 public:
   /// Retrieve the keyword associated
