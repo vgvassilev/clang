666a3bd19d653e94044188296b84c467e7c45770
Vassil Vassilev <vvasilev@cern.ch>
2014-10-01 12:12:08 +0200

    Fix cling teardown issues when a pch is attached. Kudos to Axel.

--- include/clang/Sema/Sema.h
+++ include/clang/Sema/Sema.h
@@ -150,6 +150,7 @@ namespace clang {
   typedef ArrayRef<std::pair<IdentifierInfo *, SourceLocation>> ModuleIdPath;
   class ModuleLoader;
   class MultiLevelTemplateArgumentList;
+  class MultiplexExternalSemaSource;
   class NamedDecl;
   class ObjCCategoryDecl;
   class ObjCCategoryImplDecl;
@@ -359,8 +360,9 @@ class Sema final {
   ///Source of additional semantic information.
   ExternalSemaSource *ExternalSource;
 
-  ///Whether Sema has generated a multiplexer and has to delete it.
-  bool isMultiplexExternalSource;
+  ///Source of additional semantic information if it is a multiplexer.
+  llvm::IntrusiveRefCntPtr<MultiplexExternalSemaSource>
+      MultiplexExternalSource;
 
   static bool mightHaveNonExternalLinkage(const DeclaratorDecl *FD);
 
