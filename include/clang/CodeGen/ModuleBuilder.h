628644d13b2e5f8279dfa8648886fe64b55c9568
Axel Naumann <Axel.Naumann@cern.ch>
2016-04-06 16:31:34 +0200

    From Vassil: add (debug) printout of ModuleBuilder data structures.

--- include/clang/CodeGen/ModuleBuilder.h
+++ include/clang/CodeGen/ModuleBuilder.h
@@ -20,6 +20,7 @@ namespace llvm {
   class LLVMContext;
   class Module;
   class StringRef;
+  class raw_ostream;
 }
 
 namespace clang {
@@ -83,6 +84,8 @@ public:
   ///   code generator will schedule the entity for emission if a
   ///   definition has been registered with this code generator.
   llvm::Constant *GetAddrOfGlobal(GlobalDecl decl, bool isForDefinition);
+
+  void print(llvm::raw_ostream& out);
 
   /// Create a new \c llvm::Module after calling HandleTranslationUnit. This
   /// enable codegen in interactive processing environments.
