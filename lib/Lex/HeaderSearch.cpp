fa74ea1cca703a721109b3117384ec73470dc3ed
Vassil Vassilev <v.g.vassilev@gmail.com>
2020-12-14 10:56:27 +0000

    Reinstate version of root-project/root@c9307811
    Original message:
    "
    [cxxmodules] Do not cache the file lookup failure.
    
    In cases where we have the module cache path and prebuilt module path pointing
    to the same location, the FileManager should not cache the module file lookup
    failure because it may currently be building the module.
    
    This patch is necessary because the global module index is built from the
    module cache path and it is loaded from the prebuilt module path.
    
    In a full explicit or implicit module build infrastructure this is not a problem.
    However, in a mixed scenario (where modules for third-party dependencies are
    built implicitly) such as ours this is problematic. One of the reasons is that
    we cannot configure the prebuilt modules paths or module cache paths externally.
    This is because the interpreter (at construction time) #includes RuntimeUniverse
    which may trigger module build.
    
    This patch allows us to refactor some of the code working around this issue.

--- lib/Lex/HeaderSearch.cpp
+++ lib/Lex/HeaderSearch.cpp
@@ -140,13 +140,23 @@ std::string HeaderSearch::getPrebuiltModuleFileName(StringRef ModuleName,
   if (FileMapOnly || HSOpts->PrebuiltModulePaths.empty())
     return {};
 
+
+  std::string ModuleCachePath = getModuleCachePath();
+  bool CacheFailure = true;
+
   // Then go through each prebuilt module directory and try to find the pcm
   // file.
   for (const std::string &Dir : HSOpts->PrebuiltModulePaths) {
     SmallString<256> Result(Dir);
     llvm::sys::fs::make_absolute(Result);
     llvm::sys::path::append(Result, ModuleName + ".pcm");
-    if (getFileMgr().getFile(Result.str()))
+    // If we have the same ModuleCachePath and PrebuiltModulePath pointing
+    // to the same folder we should not cache the file lookup failure as it
+    // may be currently building an implicit module.
+    if (!ModuleCachePath.empty() && ModuleCachePath == Dir)
+      CacheFailure = false;
+
+    if (getFileMgr().getFile(Result.str(), /*Open=*/false, CacheFailure))
       return Result.str().str();
   }
   return {};
